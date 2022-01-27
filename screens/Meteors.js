c8c1b32b0150b1b9172a2d9ef1a5ad33b9ebb07d
Essence14 <91456971+Essence14@users.noreply.github.com>
2022-01-27 18:16:00 +1100

    Add files via upload

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-
+import axios from 'axios';
 export default class MeteorScreen extends Component {
+    constructor(){
+        super()
+        this.state={
+            meteors:{}
+        }
+    }
+    getMeteors=()=>{
+        axios.get("https://api.nasa.gov/neo/rest/v1/feed?api_key=pcvRpSZ0Baxo1dcGKLpsadJcQQmAztiyacqJl4Te")
+        .then(Response=>{
+            this.setState({
+                meteors:Response.data.near_earth_objects
+            })
+        })
+    .catch(error=>{
+        alert(error.message)
+    })
+    }
+    componentDidMount(){
+        this.getMeteors()
+    }
     render() {
-        return (
+        if(Object.keys(this.state.meteors).length===0){
+            return(
+                <View style={styles.container}>
+                    <Text>
+                        loading...
+                    </Text>
+                </View>
+            )
+        }
+        else{
+            let meteorArr=Object.keys(this.state.meteors).map(meteor_date=>{
+                return this.state.meteors[meteor_date]
+            })
+            let meteors=[].concat.apply([],meteorArr)
+            meteors.forEach(function(element){
+                let diameter=(element.estimated_diameter.kilometers.estimated_diameter_min+element.estimated_diameter.kilometers.estimated_diameter_max)/2
+                let threatscore=(diameter/element.close_approach_data[0].miss_distance.kilometers)*1000000000
+                element.threatscore=threatscore
+            });
+            return (
             <View
                 style={{
                     flex: 1,
@@ -15,3 +54,4 @@ export default class MeteorScreen extends Component {
         )
     }
 }
+ }
